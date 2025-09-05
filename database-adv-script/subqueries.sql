60655973ec4b9098a21093cca92fe2548689ef94
michael <michaelokon565@gmail.com>
2025-09-05 13:29:25 +0100

    subquery 2

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -6,3 +6,12 @@ FROM
 	GROUP BY property_id)t
     
 WHERE av_rating > "4.0"
+
+USE airbnb_db;
+SELECT *
+FROM 
+	(SELECT sum(booking_id) total, user_id
+	FROM booking 
+	GROUP BY user_id)t
+    
+WHERE total > 3
