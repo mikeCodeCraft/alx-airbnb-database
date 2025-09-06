70b7f152bc1fb041240da086d0b68988487a1078
michael <michaelokon565@gmail.com>
2025-09-06 08:09:26 +0100

    aggre&win2

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -2,3 +2,17 @@ USE airbnb_db;
 SELECT COUNT(booking_id) total, user_id
 FROM booking 
 GROUP BY user_id
+
+WITH booking_counts AS (
+    SELECT 
+        property_id, 
+        COUNT(*) AS total_bookings
+    FROM booking
+    GROUP BY property_id
+)
+SELECT 
+    property_id,
+    total_bookings,
+    RANK() OVER (ORDER BY total_bookings DESC) AS rank1,
+    ROW_NUMBER() OVER (ORDER BY total_bookings DESC) AS row_num
+FROM booking_counts;
